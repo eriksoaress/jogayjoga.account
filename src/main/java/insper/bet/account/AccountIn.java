902d5cc416bc2a8fb2bfbee9e6b3e1fbbcb63eb2
eriksoaress <erikbtu2020@gmail.com>
2024-02-15 08:17:38 -0300

    account

---
+++ src/main/java/insper/bet/account/AccountIn.java
@@ -0,0 +1,14 @@
+package insper.bet.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder
+@Accessors(fluent = true, chain = true)
+public record AccountIn (
+    String name,
+    String email,
+    String password
+) {
+
+}
---
+++ src/main/java/insper/bet/account/AccountOut.java
@@ -0,0 +1,13 @@
+package insper.bet.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+@Builder
+@Accessors(fluent = true, chain = true)
+public record AccountOut (
+    String id,
+    String name,
+    String email
+){
+
+}
